142357cb1a97f36d71d76074695af3d54592d8c6
Wu Yuntao <wyt.brandon@gmail.com>
2017-03-01 10:16:33 +0800

    1, Merge client and common projects

--- Durian.Authentication/Properties/AssemblyInfo.cs
+++ Durian.Authentication/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Durian.Ballance.Common")]
+[assembly: AssemblyTitle("Durian.Authentication")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Durian.Ballance.Common")]
+[assembly: AssemblyProduct("Durian.Authentication")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("8edc2a3e-a606-43f0-90da-4a769fc489bb")]
+[assembly: Guid("cf8be917-b2d6-4f22-87d0-e7fcda53338a")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
--- Durian/Properties/AssemblyInfo.cs
+++ Durian/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Durian.Authentication.Common")]
+[assembly: AssemblyTitle("Durian")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Durian.Authentication.Common")]
+[assembly: AssemblyProduct("Durian")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("71418c9e-59f6-4f5a-a960-80e841cd8a87")]
+[assembly: Guid("4ea5ba80-9fef-4ff2-a026-46369d80313b")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
